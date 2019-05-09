6709e4441b2bd478c0bf6b49f0cfdf7d9d419b08
yoselalberto <yoselerratil@gmail.com>
2019-05-09 12:17:38 -0500

    add draft for data integration

---
+++ data_integration.R
@@ -0,0 +1,29 @@
+# this script integrate the diverse raw data
+
+
+# settings --------------------
+
+library(readr)
+library(igraph)
+
+
+# data ------------------------
+
+
+
+# loading --------------
+
+
+
+# cleansing  --------------
+
+
+
+# exporting   --------------
+
+
+
+
+
+
+
